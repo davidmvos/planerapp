fac236d9793eb1c0903a4e1581b6b0f541a5669e
FuchsCoder <69680700+FuchsCrafter@users.noreply.github.com>
2026-03-15 22:05:07 +0100

    Re-Login Fenster fullscreen auf Mobilgeräten machen

--- src/Settings.js
+++ src/Settings.js
@@ -136,7 +136,7 @@ export default function Settings() {
         </div>
 
         <div className="modal fade" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" id="loginModal">
-            <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-dialog modal-dialog-centered modal-fullscreen-md-down">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h1 className="modal-title fs-5" id="staticBackdropLabel">Bitte erneut anmelden</h1>
